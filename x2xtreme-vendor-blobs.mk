d65f56727f80a0c1f8646d8b8996dea95c2d53ed
bemolxd <kalkobb99@gmail.com>
2017-04-15 15:49:16 +0200

    vendor: upload vendor blobs

---
+++ x2xtreme-vendor-blobs.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/allview/x2xtreme/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/allview/x2xtreme/proprietary/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/allview/x2xtreme/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/allview/x2xtreme/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/allview/x2xtreme/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/allview/x2xtreme/proprietary/vendor,system/vendor)
